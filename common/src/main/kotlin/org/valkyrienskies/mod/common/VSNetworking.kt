23f0f649c454ba1f1e8ae1c5933fb6ad369cc023
ewoudje <ewoud.dierickx@hotmail.be>
2022-06-05 20:56:49 +0200

    More UDP :O #2

--- common/src/main/kotlin/org/valkyrienskies/mod/common/VSNetworking.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/VSNetworking.kt
@@ -5,7 +5,11 @@ import net.minecraft.server.network.ServerPlayerEntity
 import org.valkyrienskies.core.networking.IVSPacket
 import org.valkyrienskies.core.networking.IVSPacketToClientSender
 import org.valkyrienskies.core.networking.VSPacketRegistry
+import org.valkyrienskies.core.networking.impl.VSPacketSetupUDP
 import org.valkyrienskies.core.networking.impl.VSPacketShipDataList
+import org.valkyrienskies.core.networking.udp.VSUdpClient
+import org.valkyrienskies.core.networking.udp.VSUdpServer
+import org.valkyrienskies.mod.common.networking.impl.VSPacketSetupUDPClientHandler
 import org.valkyrienskies.mod.common.networking.impl.VSPacketShipDataClientHandler
 
 /**
@@ -16,12 +20,21 @@ object VSNetworking {
 
     private val vsPacketRegistry = VSPacketRegistry<ServerPlayerEntity>()
     lateinit var shipDataPacketToClientSender: IVSPacketToClientSender<ServerPlayerEntity>
+    lateinit var udpSender: VSUdpServer<ServerPlayerEntity>
+    lateinit var udpClient: VSUdpClient
 
     internal fun registerVSPackets() {
         vsPacketRegistry.registerVSPacket(
             VSPacketShipDataList::class.java,
             { VSPacketShipDataList.createEmpty() },
             VSPacketShipDataClientHandler,
+            null
+        )
+
+        vsPacketRegistry.registerVSPacket(
+            VSPacketSetupUDP::class.java,
+            { VSPacketSetupUDP.createEmpty() },
+            VSPacketSetupUDPClientHandler,
             null
         )
     }
---
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/networking/impl/VSPacketSetupUDPClientHandler.kt
@@ -0,0 +1,20 @@
+package org.valkyrienskies.mod.common.networking.impl
+
+import org.valkyrienskies.core.networking.IVSPacket
+import org.valkyrienskies.core.networking.IVSPacketClientHandler
+import org.valkyrienskies.core.networking.impl.VSPacketSetupUDP
+import org.valkyrienskies.core.networking.udp.VSUdpClient
+import org.valkyrienskies.mod.common.VSNetworking
+
+object VSPacketSetupUDPClientHandler : IVSPacketClientHandler {
+    override fun handlePacket(vsPacket: IVSPacket) {
+        vsPacket as VSPacketSetupUDP
+
+        if (!vsPacket.noUdp) {
+            val address = TODO("Get current minecraft servers address")
+            VSNetworking.udpClient = VSUdpClient(address)
+        } else {
+            TODO("Fallback to TCP")
+        }
+    }
+}
